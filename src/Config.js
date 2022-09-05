db0b1b30347ad01fa8ae82086ecfcf802bc31527
f-r00t <fruktstav@gmail.com>
2022-09-05 19:05:19 +0200

    Bumped version

--- src/Config.js
+++ src/Config.js
@@ -215,7 +215,7 @@ const Config = new function() {
     /**
      * Displayed in the settings screen
      */
-    this.appVersion = 'v1.1.3';
+    this.appVersion = 'v1.1.4';
 
     /**
      * Base URL for us to chuck a hash on the end, and find a transaction
