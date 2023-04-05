e2cdd2541d7442c39b64991ff48225f10f41e63a
f-r00t <fruktstav@gmail.com>
2023-04-05 22:41:53 +0200

    Bumped version

--- android/app/src/main/java/com/hugin/MainApplication.java
+++ android/app/src/main/java/com/hugin/MainApplication.java
@@ -66,7 +66,7 @@ public class MainApplication extends Application implements ReactApplication {
     upgradeSecurityProvider();
 
     /* tonchan-vx.x.x */
-    setUserAgent("hugin-messenger-v1.1.8");
+    setUserAgent("hugin-messenger-v1.2.0");
 
     SoLoader.init(this, /* native exopackage */ false);
   }
