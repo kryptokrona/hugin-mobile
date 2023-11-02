b07062cab42780320c0f9f31aed03518a36f296a
f-r00t <fruktstav@gmail.com>
2023-11-02 20:44:25 +0100

    Change from active to actpass in sdp

--- src/SDPParser.js
+++ src/SDPParser.js
@@ -133,7 +133,7 @@ a=ice-pwd:` +
 a=fingerprint:sha-256 ` +
         fingerprint +
         `
-a=setup:active
+a=setup:actpass
 a=mid:data
 a=sctp-port:5000
 a=max-message-size:262144
@@ -266,7 +266,7 @@ a=ice-pwd:` +
 a=fingerprint:sha-256 ` +
     fingerprint +
     `
-a=setup:passive
+a=setup:active
 a=mid:data
 a=sctp-port:5000
 a=max-message-size:262144
