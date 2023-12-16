eaf3f119661e056717d5bef07dfdeb0b2972d29e
Dnyaneshwar  Rede <79148974+dnyaneshwar-rede@users.noreply.github.com>
2023-12-16 19:07:13 +0000

    Translation in Marathi language added

--- src/i18n.js
+++ src/i18n.js
@@ -11,6 +11,7 @@ import fi from './i18n/fi.json';
 import pt from './i18n/pt.json';
 import uk from './i18n/uk.json';
 import hi from './i18n/hi.json';
+import mr from './i18n/mr.json';
 import { Globals } from './Globals';
 import RNLanguageDetector from '@os-team/i18next-react-native-language-detector';
 
@@ -64,6 +65,9 @@ i18next
       },
       hi: {
         translation: hi,
+      },
+      mr: {
+        translation: mr,
       }
     },
   });
